9d7cffef7b2b8fee08611a158b768a4198367d7d
Kostas <diamantopoulos.kostas@hotmail.com>
2024-11-17 17:15:53 +0200

    added more comments in jest-test file

--- __tests__/page.test.jsx
+++ __tests__/page.test.jsx
@@ -1,20 +1,24 @@
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import Page from '../src/app/page';
+import '@testing-library/jest-dom';             // Provides custom jest matchers for DOM node assertions
+import { render, screen } from '@testing-library/react';          // Tools for rendering components and querying the DOM
+import Page from '../src/app/page';      // Import the Page component to be tested
 
 
-// Mocking fetch
+
+// Mocking the global fetch function to simulate API responses during tests
 global.fetch = jest.fn(() =>
   Promise.resolve({
-    json: () => Promise.resolve({ rate: 1.19 }), // Simulate a successful API response
+    json: () => Promise.resolve({ rate: 1.19 }),   // Simulate a successful API response with a rate of 1.19
   })
 );
 
 
+// Test suite for the Page component
 describe('Page', () => {
+
+  // Test case to verify that the heading is rendered
   it('renders a heading', () => {
-    render(<Page />);
-    const heading = screen.getByRole('heading', { level: 1 });
-    expect(heading).toBeInTheDocument();
+    render(<Page />);               // Render the Page component
+    const heading = screen.getByRole('heading', { level: 1 });  // Query for the heading element with level 1
+    expect(heading).toBeInTheDocument();                       // Assert that the heading is present in the document
   });
 });
