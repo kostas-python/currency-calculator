68214cb40d0267a1bf0214b2a88cd448c90fd921
Kostas <diamantopoulos.kostas@hotmail.com>
2025-01-18 00:27:50 +0200

    hash updated

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -21,7 +21,7 @@ export default function LoginPage() {
   // Admin credentials (hashed password for 'password')
   const adminCredentials = {
     email: process.env.NEXT_PUBLIC_ADMIN_EMAIL ?? '', 
-    passwordHash: "$2a$12$Teb8wOTF2rL6dDjfFJMRcOM84S0Tco43c2xovlAEqjDm1Jmz2f/3i" 
+    passwordHash: '',
   };
 
   // Handle form submission
